c0a8bfb5ef4f85b1c8779e9b2722da86923fb057
Mussie Equbatsion Mesfin <mussie009@users.noreply.github.com>
2018-06-14 11:17:18 +0200

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,8 +5,6 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="mb-5 mt-2">
-            Codegarden's Test
-            
             <a href="/form" className="float-sm-right">create user</a>
             <hr />
         </div>
